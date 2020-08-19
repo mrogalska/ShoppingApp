b95d5bf6da84936a0b68546479996f06e82d11c9
m.rogalska <m.rogalska@wasko.pl>
2020-08-19 16:16:41 +0200

    fix ProductsComponents and ProductService

--- guiShopping/src/app/application/services/product.service.ts
+++ guiShopping/src/app/application/services/product.service.ts
@@ -1,66 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { Product } from '../domain/external/product';;
+import { Product } from '../domain/external/product';
+import { environment } from "../../../environments/environment";
 
+
+const serverUrl = 'http://localhost:8090/products';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
+  })
+};
 
 @Injectable({ providedIn: 'root' })
 @Injectable()
 export class ProductService {
 
-  // pola prywatne zaczynamy od _
-  private productsUrl: string;
+  constructor(private http: HttpClient) { }
 
-  //  jako const
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json; charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT'
-    })
-  };
-
-  constructor(
-    private http: HttpClient
-  ) {
-    //  Nie możesz zdeklarować tego jako stała, a najlepiej w envorimentsach?
-    this.productsUrl = 'http://localhost:8090/products';
-  }
 
   getProducts(): Observable<Product[]> {
-    const url = `${this.productsUrl}/getAll`;
+    const url = `${serverUrl}/getAll`;
     return this.http.get<Product[]>(url);
   }
 
 
   getProduct(id: number): Observable<Product> {
-    const url = `${this.productsUrl}/getById/${id}`;
+    const url = `${serverUrl}/getById/${id}`;
     return this.http.get<Product>(url);
   }
 
 
   addProduct(product: Product) {
-    const url = `${this.productsUrl}/add`;
-    return this.http.post<Product>(url, product, {
-      headers: new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8')
-    });
+    const url = `${serverUrl}/add`;
+    return this.http.post<Product>(url, product, httpOptions);
   }
 
 
-  updateProduct(product: Product | number): Observable<any> {
-    // hmm po co to? Nie wiesz co przesyłasz?
-    const id = typeof product === 'number' ? product : product.id;
-    const url = `${this.productsUrl}/update/${id}`;
-    return this.http.put<Product>(url, product, this.httpOptions);
+  updateProduct(product: Product): Observable<Product> {
+    const id = product.id;
+    const url = `${serverUrl}/update/${id}`;
+    return this.http.put<Product>(url, product, httpOptions);
   }
 
 
-  deleteProduct(product: Product | number): Observable<Product> {
-    // hmm po co to? Nie wiesz co przesyłasz?
-    const id = typeof product === 'number' ? product : product.id;
-    const url = `${this.productsUrl}/delete/${id}`;
-    return this.http.delete<Product>(url, this.httpOptions);
+  deleteProduct(product: Product): Observable<Product> {
+    const id = product.id;
+    const url = `${serverUrl}/delete/${id}`;
+    return this.http.delete<Product>(url, httpOptions);
   }
 
 }
--- guiShopping/src/app/application/views/products/products.component.ts
+++ guiShopping/src/app/application/views/products/products.component.ts
@@ -9,26 +9,64 @@ import { ProductService } from '../../services/product.service';
 })
 export class ProductsComponent implements OnInit {
 
-  // Pola prywatne, enkapsulacja danych
-  displayedColumns: string[] = ['name', 'category', 'price', 'id'];
-  products: Product[];
-  showOptions: number = 0;
-  pricesArray: number[];
+  private _displayedColumns: string[] = ['name', 'category', 'price', 'id'];
+  private _products: Product[];
+  private _showOptions: number = 0;
+  private _pricesArray: number[];
 
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    // osobna funkcja
+    this.getProducts();
+
+  }
+
+  getProducts() {
     this.productService.getProducts().subscribe(data => {
-      this.products = data;
+      this._products = data;
     })
   }
 
-  //  czemu tu bezpośrednio id nie przysyłasz?
-  delete(product: Product): void {
-    this.products = this.products.filter(h => h !== product);
-    this.productService.deleteProduct(product).subscribe();
+    //  czemu tu bezpośrednio id nie przysyłasz?
+    delete(product: Product): void {
+      this._products = this._products.filter(h => h !== product);
+      this.productService.deleteProduct(product).subscribe();
+    }
+
+
+  // getters and setters
+
+  public get $displayedColumns() {
+    return this._displayedColumns;
+  }
+
+  public set $displayedColumns(value: string[]) {
+    this._displayedColumns = value;
+  }
+
+  public get $products() {
+    return this._products;
+  }
+
+  public set $products(value: Product[]) {
+    this._products = value;
+  }
+
+  public get $showOptions() {
+    return this._showOptions;
+  }
+
+  public set $showOptions(value: number) {
+    this._showOptions = value;
+  }
+
+  public get $pricesArray() {
+    return this._pricesArray;
+  }
+
+  public set $pricesArray(value: number[]) {
+    this._pricesArray = value;
   }
 
 }
